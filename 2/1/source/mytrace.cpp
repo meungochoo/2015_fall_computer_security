281bcea8628c62ebc78ccee43f7588396e3f0fcf
Taewon Park <demian51.tw@gmail.com>
2015-10-15 02:59:44 +0900

    Implemented basic syscall tracing
    Printing all syscall called by child process.
    Only can trace syscall id and its return value yet. Need additional
    implementation...

--- 2/1/source/mytrace.cpp
+++ 2/1/source/mytrace.cpp
@@ -4,28 +4,86 @@
 
 #include <boost/program_options.hpp>
 
-#include <getopt.h>
 #include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/syscall.h>
 #include <sys/types.h>
+#include <sys/user.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-//#include "messages.h"
-
 namespace po = boost::program_options;
 
-int run_child(const std::string& path, char* argv[])
+bool run_child(const std::string& path, char* argv[])
 {
 	if(ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) == -1)
 	{
 		// Some error happend with ptrace
-		return -1;
+		return false;
 	}
 
 	execvp(path.c_str(), argv);
 
 	// Failed to execute
-	return -1;
+	return false;
+}
+
+bool continue_trace_syscall(int child_pid)
+{
+	if(ptrace(PTRACE_SYSCALL, child_pid, nullptr, nullptr) == -1)
+	{
+		std::cerr << "Error : Failed to continue tracing." << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+int do_trace(int child_pid, po::variables_map& vm)
+{
+	int status;
+	bool is_in_syscall = false, is_child_executed = false;
+	user_regs_struct regs;
+
+	// Trace syscalls until child process ends
+	while(true)
+	{
+		waitpid(child_pid, &status, 0);
+
+		if(WIFEXITED(status) || WIFSIGNALED(status))
+		{
+			break;
+		}
+
+		is_in_syscall = !is_in_syscall;
+
+		if(ptrace(PTRACE_GETREGS, child_pid, nullptr, &regs) == -1)
+		{
+			std::cerr << "Error : Failed to get register info." << std::endl;
+			break;
+		}
+
+		// TODO : Ignore syscalls until function main is called
+		
+		// TODO : Implement additional features...
+		if(!is_in_syscall)
+		{
+			// Entered to syscall: modify syscall number if needed
+		}
+		else
+		{
+			// Returned from syscall: get return value
+			std::cerr << "syscall[" << regs.orig_rax << "](unimplemented) = ";
+			fprintf(stderr, (static_cast<long long>(regs.rax) > 1024 ? "0x%llx\n" : "%lld\n"), static_cast<long long>(regs.rax));
+		}
+
+		if(!continue_trace_syscall(child_pid))
+		{
+			break;
+		}
+	}
+
+	return 0;
 }
 
 po::variables_map parse_program_options(int argn, char* argv[])
@@ -89,11 +147,18 @@ int main(int argn, char* argv[])
 		args.push_back(nullptr);
 
 		// Run given process
-		return run_child(args_vec.front(), args.data());
+		if(!run_child(args_vec.front(), args.data()))
+		{
+			std::cerr << "Error : Failed to execute and trace the given commands." << std::endl;
+		}
+	}
+	else if(child_pid == -1)
+	{
+		std::cerr << "Error : Failed to execute the given commands." << std::endl;
 	}
 	else
 	{
-		// Trace it! fuck yeah!	
+		do_trace(child_pid, vm);
 	}
 
 	return 0;
